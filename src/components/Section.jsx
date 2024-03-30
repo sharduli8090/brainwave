00a7cb90c9eac23a2614b4b8e6b1938b61addbef
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-03-30 16:31:52 +0530

    Header created

---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section = ({className, id, crosses, crossesOffset, customPaddings,children}) => {
+  return (
+    <div>Section</div>
+  )
+}
+
+export default Section
